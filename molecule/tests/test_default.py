a58daa99dfb7c452f33ec0263783bee008fd6e55
Оберест Алексей <aleksey.oberest@gazprombank.ru>
2024-03-21 20:40:56 +0600

    add steps for deploy on redhat base os

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -8,7 +8,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
-
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+@pytest.mark.parametrize('pkg', [
+'nginx',
+'redis'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
+
+@pytest.mark.parametrize('file, content', [
+("/etc/nginx/conf.d/funkwhale.conf", "upstream funkwhale-api"),
+("/etc/systemd/system/funkwhale-server.service", "User=funkwhale")
+])
+def test_files(host, file, content):
+    file = host.file(file)
+    assert file.exists
+    assert file.contains(content)
